5b1741d348af4966e8e22556fcaa2662cdfa738f
Ryan McClue <re.mcclue@protonmail.com>
2021-09-16 13:59:34 +1000

    Refactor back buffer by value to by reference to avoid compiler inline assumptions

--- code/ubuntu-hhf.cpp
+++ code/ubuntu-hhf.cpp
@@ -168,15 +168,15 @@ xlib_display_back_buffer(Display *display, XRenderPictFormat *format, Window win
 }
 
 INTERNAL void
-render_weird_gradient(XlibBackBuffer back_buffer, int x_offset, int y_offset)
+render_weird_gradient(XlibBackBuffer *back_buffer, int x_offset, int y_offset)
 {
-  u32 *pixel = (u32 *)back_buffer.memory;
+  u32 *pixel = (u32 *)back_buffer->memory;
   for (int back_buffer_y = 0; 
-        back_buffer_y < back_buffer.height;
+        back_buffer_y < back_buffer->height;
         ++back_buffer_y)
   {
     for (int back_buffer_x = 0; 
-        back_buffer_x < back_buffer.width;
+        back_buffer_x < back_buffer->width;
         ++back_buffer_x)
     {
       u8 red = back_buffer_x + x_offset;
@@ -577,7 +577,7 @@ main(int argc, char *argv[])
       }
     }
 
-    render_weird_gradient(xlib_back_buffer, x_offset, y_offset);
+    render_weird_gradient(&xlib_back_buffer, x_offset, y_offset);
 
     xlib_display_back_buffer(xlib_display, xrender_pic_format, xlib_window,
                              xlib_gc, xlib_back_buffer, xlib_window_width, 
