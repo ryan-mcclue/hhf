ca2ef9363b211829ca683698a3af3a0d2713f438
Ryan McClue <re.mcclue@protonmail.com>
2021-10-25 12:37:52 +1100

    Add initial support for multiple tilemaps

--- code/hhf.cpp
+++ code/hhf.cpp
@@ -93,6 +93,42 @@ draw_rect(HHFBackBuffer *back_buffer, r32 x0, r32 y0, r32 x1, r32 y1, r32 r, r32
 
 }
 
+struct TileMap
+{
+  int num_tiles_x;
+  int num_tiles_y;
+
+  r32 upper_left_x;
+  r32 upper_left_y;
+  r32 tile_width;
+  r32 tile_height;
+
+  u32 *tiles;
+};
+
+INTERNAL inline u32
+get_tile_map_value_unchecked(TileMap *tile_map, int x, int y)
+{
+  return tile_map->tiles[y * tile_map->num_tiles_x + x];
+}
+
+INTERNAL bool
+is_tile_map_point_empty(TileMap *tile_map, r32 x, r32 y)
+{
+  int player_tile_x = truncate_r32_to_int((x - tile_map->upper_left_x) / tile_map->tile_width);
+  int player_tile_y = truncate_r32_to_int((y - tile_map->upper_left_y) / tile_map->tile_height);
+
+  bool is_tile_empty = false;
+  if (player_tile_x >= 0 && player_tile_x < tile_map->num_tiles_x &&
+      player_tile_y >= 0 && player_tile_y < tile_map->num_tiles_y)
+  {
+    u32 tile_map_value = get_tile_map_value_unchecked(tile_map, player_tile_x, player_tile_y);
+    is_tile_empty = (tile_map_value == 0);
+  }
+
+  return is_tile_empty;
+}
+
 extern "C" void
 hhf_update_and_render(HHFThreadContext *thread_context, HHFBackBuffer *back_buffer, 
                       HHFSoundBuffer *sound_buffer, HHFInput *input, HHFMemory *memory, 
@@ -103,32 +139,59 @@ hhf_update_and_render(HHFThreadContext *thread_context, HHFBackBuffer *back_buff
   HHFState *state = (HHFState *)memory->permanent;
   if (!memory->is_initialized)
   {
-    state->player_x = 100;
-    state->player_y = 100;
+    state->player_x = 300;
+    state->player_y = 300;
     memory->is_initialized = true;
   }
 
-#define TILE_MAP_NUM_TILES_X 16
-#define TILE_MAP_NUM_TILES_Y 9
-  u32 tile_map[TILE_MAP_NUM_TILES_Y][TILE_MAP_NUM_TILES_X] =
+  #define TILE_MAP_NUM_TILES_X 16
+  #define TILE_MAP_NUM_TILES_Y 9
+  u32 tile_map_tiles0[TILE_MAP_NUM_TILES_Y][TILE_MAP_NUM_TILES_X] =
   {
-    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-    {0, 0, 1, 1, 1, 1, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0},
-    {0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0},
-    {0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0},
-    {0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0},
-    {1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0},
-    {1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0},
-    {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0},
-    {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0},
+    {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+    {1, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1},
+    {1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+    {1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+    {1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+    {1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1},
+    {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 1},
+    {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1},
+    {1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1},
+  };
+  u32 tile_map_tiles1[TILE_MAP_NUM_TILES_Y][TILE_MAP_NUM_TILES_X] =
+  {
+    {1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1},
+    {1, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1},
+    {1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+    {1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+    {1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+    {1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1},
+    {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 1},
+    {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1},
+    {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
   };
 
-  r32 tile_width = 75.0f;
-  r32 tile_height = 75.0f;
-
+  TileMap tile_maps[2];
+  tile_maps[0].num_tiles_x = TILE_MAP_NUM_TILES_X;
+  tile_maps[0].num_tiles_y = TILE_MAP_NUM_TILES_Y;
+  tile_maps[0].tile_width = 75.0f;
+  tile_maps[0].tile_height = 75.0f;
   // NOTE(Ryan): Useful to have these to only show half of a tile, etc.
-  r32 upper_left_x = 0.0f;
-  r32 upper_left_y = 0.0f;
+  tile_maps[0].upper_left_x = 0.0f;
+  tile_maps[0].upper_left_y = 0.0f;
+  tile_maps[0].tiles = (u32 *)tile_map_tiles0;
+ 
+  // TODO(Ryan): The fact we are cloning is suspicious
+  tile_maps[1] = tile_maps[0];
+  tile_maps[1].tiles = (u32 *)tile_map_tiles1;
+
+  TileMap *active_tile_map = &tile_maps[0];
+
+  r32 player_r = 0.5f;
+  r32 player_g = 0.2f;
+  r32 player_b = 1.0f;
+  r32 player_width = 0.75f * active_tile_map->tile_width;
+  r32 player_height = active_tile_map->tile_height;
 
   // counting how many half transition counts over say half a second gives us
   // whether the user 'dashed'
@@ -153,22 +216,16 @@ hhf_update_and_render(HHFThreadContext *thread_context, HHFBackBuffer *back_buff
         if (controller.action_up.ended_down) dplayer_y = -1.0f;
         if (controller.action_down.ended_down) dplayer_y = 1.0f;
 
-        dplayer_x *= 64.0f;
-        dplayer_y *= 64.0f;
+        dplayer_x *= 128.0f;
+        dplayer_y *= 128.0f;
 
         r32 new_player_x = state->player_x + (dplayer_x * input->frame_dt);
         r32 new_player_y = state->player_y + (dplayer_y * input->frame_dt);
+        
+        bool is_tile_valid = is_tile_map_point_empty(active_tile_map, new_player_x, new_player_y) &&
+          is_tile_map_point_empty(active_tile_map, new_player_x + player_width * 0.5f, new_player_y) &&
+          is_tile_map_point_empty(active_tile_map, new_player_x - player_width * 0.5f, new_player_y);
 
-        int player_tile_x = truncate_r32_to_int((new_player_x - upper_left_x) / tile_width);
-        int player_tile_y = truncate_r32_to_int((new_player_y - upper_left_y) / tile_height);
-
-        bool is_tile_valid = false;
-        if (player_tile_x >= 0 && player_tile_x < TILE_MAP_NUM_TILES_X &&
-            player_tile_y >= 0 && player_tile_y < TILE_MAP_NUM_TILES_Y)
-        {
-          u32 tile_map_value = tile_map[player_tile_y][player_tile_x];
-          is_tile_valid = (tile_map_value == 0);
-        }
 
         if (is_tile_valid)
         {
@@ -181,31 +238,25 @@ hhf_update_and_render(HHFThreadContext *thread_context, HHFBackBuffer *back_buff
     }
   }
 
-
   draw_rect(back_buffer, 0, 0, back_buffer->width, back_buffer->height, 1.0f, 0.0f, 1.0f);
 
   for (int y = 0; y < 9; ++y)
   {
     for (int x = 0; x < 16; ++x)
     {
-      u32 tile_id = tile_map[y][x];
+      u32 tile_id = get_tile_map_value_unchecked(active_tile_map, x, y);
       r32 grayscale = 0.5f;
       if (tile_id == 1) grayscale = 1.0f;
 
-      r32 min_x = upper_left_x + ((r32)x * tile_width);
-      r32 min_y = upper_left_y + ((r32)y * tile_height);
-      r32 max_x = min_x + tile_width;
-      r32 max_y = min_y + tile_height;
+      r32 min_x = active_tile_map->upper_left_x + ((r32)x * active_tile_map->tile_width);
+      r32 min_y = active_tile_map->upper_left_y + ((r32)y * active_tile_map->tile_height);
+      r32 max_x = min_x + active_tile_map->tile_width;
+      r32 max_y = min_y + active_tile_map->tile_height;
 
       draw_rect(back_buffer, min_x, min_y, max_x, max_y, grayscale, grayscale, grayscale);
     }
   }
 
-  r32 player_r = 0.5f;
-  r32 player_g = 0.2f;
-  r32 player_b = 1.0f;
-  r32 player_width = 0.75f * tile_width;
-  r32 player_height = tile_height;
 
   r32 player_min_x = state->player_x - (player_width * 0.5f);
   r32 player_min_y = state->player_y - player_height;
